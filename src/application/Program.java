0ddb969858c0925d6cd086f927d66b80389be70b
Pabbraga <pabbragadev@gmail.com>
2024-04-27 19:13:45 -0300

    feat: implement insert seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -28,6 +29,18 @@ public class Program {
         for (Seller sel : list) {
             System.out.println(sel);
         }
+
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(
+                null,
+                "Greg",
+                "greg@gmail.com",
+                new Date(),
+                4000.0,
+                new Department(2, null)
+        );
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! new id = " + newSeller.getId());
     }
 
 }
